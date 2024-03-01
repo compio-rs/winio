85dbff24c3d01e81209347cf47c0fc0259fa0757
王宇逸 <Strawberry_Str@hotmail.com>
2024-03-02 02:36:35 +0800

    feat(gtk): fill canvas

--- src/ui/gtk/canvas.rs
+++ src/ui/gtk/canvas.rs
@@ -1,17 +1,21 @@
 use std::{
+    f64::consts::{FRAC_PI_2, PI},
     io,
     rc::{Rc, Weak},
 };
 
 use glib::object::Cast;
 use gtk4::{
-    cairo::Context,
+    cairo::{Context, FontSlant, FontWeight},
     prelude::{DrawingAreaExtManual, WidgetExt},
     DrawingArea,
 };
 
 use super::callback::Callback;
-use crate::{AsContainer, Container, Point, Size, Widget};
+use crate::{
+    AsContainer, BrushPen, Container, DrawingFont, HAlign, Point, Rect, RectBox, RelativeToScreen,
+    Size, SolidColorBrush, VAlign, Widget,
+};
 
 pub struct Canvas {
     widget: DrawingArea,
@@ -79,6 +83,246 @@ pub struct DrawingContext {
     ctx: Context,
 }
 
-pub trait Brush {}
+#[inline]
+fn to_trans(rect: Rect) -> RelativeToScreen {
+    RelativeToScreen::scale(rect.size.width, rect.size.height)
+        .then_translate(rect.origin.to_vector())
+}
 
-pub trait Pen {}
+impl DrawingContext {
+    #[inline]
+    fn set_brush(&self, brush: impl Brush, rect: Rect) {
+        brush.set(&self.ctx, to_trans(rect))
+    }
+
+    #[inline]
+    fn set_pen(&self, pen: impl Pen, rect: Rect) {
+        pen.set(&self.ctx, to_trans(rect))
+    }
+
+    fn path_arc(&self, rect: Rect, start: f64, end: f64) {
+        let save_matrix = self.ctx.matrix();
+        let rate = rect.size.height / rect.size.width;
+        self.ctx.scale(1.0, rate);
+        self.ctx.new_path();
+        let center = rect.center();
+        self.ctx
+            .arc(center.x, center.y / rate, rect.size.width / 2.0, start, end);
+        self.ctx.set_matrix(save_matrix);
+    }
+
+    fn path_rect(&self, rect: Rect) {
+        self.ctx.new_path();
+        self.ctx.rectangle(
+            rect.origin.x,
+            rect.origin.y,
+            rect.size.width,
+            rect.size.height,
+        );
+    }
+
+    fn path_round_rect(&self, rect: Rect, round: Size) {
+        let save_matrix = self.ctx.matrix();
+        self.ctx.scale(1.0, round.height / round.width);
+        self.ctx.new_sub_path();
+        self.ctx.arc(
+            rect.origin.x + rect.size.width - round.width,
+            rect.origin.y + round.height,
+            round.width,
+            -FRAC_PI_2,
+            0.0,
+        );
+        self.ctx.arc(
+            rect.origin.x + rect.size.width - round.width,
+            rect.origin.y + rect.size.height - round.height,
+            round.width,
+            0.0,
+            FRAC_PI_2,
+        );
+        self.ctx.arc(
+            rect.origin.x + round.width,
+            rect.origin.y + rect.size.height - round.height,
+            round.width,
+            FRAC_PI_2,
+            PI,
+        );
+        self.ctx.arc(
+            rect.origin.x + round.width,
+            rect.origin.y + round.height,
+            round.width,
+            PI,
+            FRAC_PI_2 * 3.0,
+        );
+        self.ctx.close_path();
+        self.ctx.set_matrix(save_matrix);
+    }
+
+    pub fn draw_arc(&self, pen: impl Pen, rect: Rect, start: f64, end: f64) -> io::Result<()> {
+        self.path_arc(rect, start, end);
+        self.set_pen(pen, rect);
+        self.ctx
+            .stroke()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn fill_pie(&self, brush: impl Brush, rect: Rect, start: f64, end: f64) -> io::Result<()> {
+        self.path_arc(rect, start, end);
+        self.set_brush(brush, rect);
+        self.ctx
+            .fill()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn draw_ellipse(&self, pen: impl Pen, rect: Rect) -> io::Result<()> {
+        self.draw_arc(pen, rect, 0.0, PI * 2.0)
+    }
+
+    pub fn fill_ellipse(&self, brush: impl Brush, rect: Rect) -> io::Result<()> {
+        self.fill_pie(brush, rect, 0.0, PI * 2.0)
+    }
+
+    pub fn draw_line(&self, pen: impl Pen, start: Point, end: Point) -> io::Result<()> {
+        let rect = RectBox::new(
+            Point::new(start.x.min(end.x), start.y.min(end.y)),
+            Point::new(start.x.max(end.x), start.y.max(end.y)),
+        )
+        .to_rect();
+        self.ctx.new_path();
+        self.ctx.move_to(start.x, start.y);
+        self.ctx.line_to(end.x, end.y);
+        self.set_pen(pen, rect);
+        self.ctx
+            .stroke()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn draw_rect(&self, pen: impl Pen, rect: Rect) -> io::Result<()> {
+        self.path_rect(rect);
+        self.set_pen(pen, rect);
+        self.ctx
+            .stroke()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn fill_rect(&self, brush: impl Brush, rect: Rect) -> io::Result<()> {
+        self.path_rect(rect);
+        self.set_brush(brush, rect);
+        self.ctx
+            .fill()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn draw_round_rect(&self, pen: impl Pen, rect: Rect, round: Size) -> io::Result<()> {
+        self.path_round_rect(rect, round);
+        self.set_pen(pen, rect);
+        self.ctx
+            .stroke()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn fill_round_rect(&self, brush: impl Brush, rect: Rect, round: Size) -> io::Result<()> {
+        self.path_round_rect(rect, round);
+        self.set_brush(brush, rect);
+        self.ctx
+            .fill()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    pub fn draw_str(
+        &self,
+        brush: impl Brush,
+        font: DrawingFont,
+        pos: Point,
+        text: impl AsRef<str>,
+    ) -> io::Result<()> {
+        let text = text.as_ref();
+        let rect = self.measure_str(font, pos, text)?;
+        self.ctx
+            .move_to(rect.origin.x, rect.origin.y + rect.size.height);
+        self.set_brush(brush, rect);
+        self.ctx
+            .show_text(text)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        Ok(())
+    }
+
+    fn measure_str(
+        &self,
+        font: DrawingFont,
+        pos: Point,
+        text: impl AsRef<str>,
+    ) -> io::Result<Rect> {
+        self.ctx.select_font_face(
+            &font.family,
+            if font.italic {
+                FontSlant::Italic
+            } else {
+                FontSlant::Normal
+            },
+            if font.bold {
+                FontWeight::Bold
+            } else {
+                FontWeight::Normal
+            },
+        );
+        self.ctx.set_font_size(font.size);
+        let ex = self
+            .ctx
+            .text_extents(text.as_ref())
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        let mut x = pos.x;
+        let mut y = pos.y;
+        match font.halign {
+            HAlign::Center => x -= ex.width() / 2.0,
+            HAlign::Right => x -= ex.width(),
+            _ => {}
+        }
+        match font.valign {
+            VAlign::Center => y += ex.height() / 2.0,
+            VAlign::Top => y += ex.height(),
+            _ => {}
+        }
+        Ok(Rect::new(
+            Point::new(x, y - ex.height()),
+            Size::new(ex.width(), ex.height()),
+        ))
+    }
+}
+
+pub trait Brush {
+    fn set(&self, ctx: &Context, trans: RelativeToScreen);
+}
+
+impl<B: Brush> Brush for &'_ B {
+    fn set(&self, ctx: &Context, trans: RelativeToScreen) {
+        (**self).set(ctx, trans)
+    }
+}
+
+impl Brush for SolidColorBrush {
+    fn set(&self, ctx: &Context, _trans: RelativeToScreen) {
+        ctx.set_source_rgba(
+            self.color.r as f64 / 255.0,
+            self.color.g as f64 / 255.0,
+            self.color.b as f64 / 255.0,
+            self.color.a as f64 / 255.0,
+        );
+    }
+}
+
+pub trait Pen {
+    fn set(&self, ctx: &Context, trans: RelativeToScreen);
+}
+
+impl<P: Pen> Pen for &'_ P {
+    fn set(&self, ctx: &Context, trans: RelativeToScreen) {
+        (**self).set(ctx, trans)
+    }
+}
+
+impl<B: Brush> Pen for BrushPen<B> {
+    fn set(&self, ctx: &Context, trans: RelativeToScreen) {
+        self.brush.set(ctx, trans);
+        ctx.set_line_width(self.width);
+    }
+}
