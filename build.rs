1facde897de34eda6595c7c806dadd7289d6d182
王宇逸 <Strawberry_Str@hotmail.com>
2024-10-10 04:16:08 +0900

    fix(qt): force C++ 20

--- build.rs
+++ build.rs
@@ -37,6 +37,7 @@ fn main() {
 
         let mut build = cxx_build::bridges(sources.map(|s| format!("{}.rs", s)));
         build
+            .std("c++20")
             .files(sources.map(|s| format!("{}.cpp", s)))
             .includes(inc);
         if std::env::var("PROFILE").as_deref() == Ok("release") {
