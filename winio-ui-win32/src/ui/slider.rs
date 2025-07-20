936570661f38679258e5cca6e79e06d16a27f9f1
王宇逸 <Strawberry_Str@hotmail.com>
2025-07-21 01:21:22 +0800

    feat(win32): slider

---
+++ winio-ui-win32/src/ui/slider.rs
@@ -0,0 +1,222 @@
+use inherit_methods_macro::inherit_methods;
+use windows_sys::Win32::UI::{
+    Controls::{
+        TBM_GETRANGEMAX, TBM_GETRANGEMIN, TBM_SETPAGESIZE, TBM_SETPOSNOTIFY, TBM_SETRANGEMAX,
+        TBM_SETRANGEMIN, TBS_AUTOTICKS, TBS_BOTH, TBS_HORZ, TBS_TOOLTIPS, TBS_VERT,
+        TRACKBAR_CLASSW,
+    },
+    WindowsAndMessaging::{WM_HSCROLL, WM_USER, WM_VSCROLL, WS_CHILD, WS_TABSTOP, WS_VISIBLE},
+};
+use winio_handle::{AsRawWidget, AsWindow, RawWidget};
+use winio_primitive::{Orient, Point, Size};
+
+use crate::Widget;
+
+#[derive(Debug)]
+struct SliderImpl {
+    handle: Widget,
+}
+
+#[inherit_methods(from = "self.handle")]
+impl SliderImpl {
+    pub fn new(parent: impl AsWindow, style: u32) -> Self {
+        let mut handle = Widget::new(
+            TRACKBAR_CLASSW,
+            WS_CHILD | WS_TABSTOP | TBS_AUTOTICKS | TBS_BOTH | TBS_TOOLTIPS | style,
+            0,
+            parent.as_window().as_win32(),
+        );
+        handle.set_size(handle.size_d2l((50, 14)));
+        handle.send_message(TBM_SETPAGESIZE, 0, 1);
+        Self { handle }
+    }
+
+    pub fn is_visible(&self) -> bool;
+
+    pub fn set_visible(&mut self, v: bool);
+
+    pub fn is_enabled(&self) -> bool;
+
+    pub fn set_enabled(&mut self, v: bool);
+
+    pub fn loc(&self) -> Point;
+
+    pub fn set_loc(&mut self, p: Point);
+
+    pub fn size(&self) -> Size;
+
+    pub fn set_size(&mut self, v: Size);
+
+    pub fn range(&self) -> (usize, usize) {
+        let min = self.handle.send_message(TBM_GETRANGEMIN, 0, 0);
+        let max = self.handle.send_message(TBM_GETRANGEMAX, 0, 0);
+        (min as _, max as _)
+    }
+
+    pub fn set_range(&mut self, min: usize, max: usize) {
+        self.handle.send_message(TBM_SETRANGEMIN, 0, min as _);
+        // only redraw when both values are set
+        self.handle.send_message(TBM_SETRANGEMAX, 1, max as _);
+    }
+
+    pub fn pos(&self) -> usize {
+        // Why is it not in `windows-sys`?
+        const TBM_GETPOS: u32 = WM_USER;
+        self.handle.send_message(TBM_GETPOS, 0, 0) as _
+    }
+
+    pub fn set_pos(&mut self, v: usize) {
+        self.handle.send_message(TBM_SETPOSNOTIFY, 0, v as _);
+    }
+}
+
+impl AsRawWidget for SliderImpl {
+    fn as_raw_widget(&self) -> RawWidget {
+        self.handle.as_raw_widget()
+    }
+}
+
+#[derive(Debug)]
+pub struct Slider {
+    handle: SliderImpl,
+    vhandle: SliderImpl,
+    vertical: bool,
+}
+
+impl Slider {
+    pub fn new(parent: impl AsWindow) -> Self {
+        let parent = parent.as_window();
+        let handle = SliderImpl::new(&parent, WS_VISIBLE | TBS_HORZ);
+        let vhandle = SliderImpl::new(&parent, TBS_VERT);
+        Self {
+            handle,
+            vhandle,
+            vertical: false,
+        }
+    }
+
+    pub fn is_visible(&self) -> bool {
+        if self.vertical {
+            &self.vhandle
+        } else {
+            &self.handle
+        }
+        .is_visible()
+    }
+
+    pub fn set_visible(&mut self, v: bool) {
+        if self.vertical {
+            &mut self.vhandle
+        } else {
+            &mut self.handle
+        }
+        .set_visible(v);
+    }
+
+    pub fn is_enabled(&self) -> bool {
+        self.handle.is_enabled()
+    }
+
+    pub fn set_enabled(&mut self, v: bool) {
+        self.handle.set_enabled(v);
+        self.vhandle.set_enabled(v);
+    }
+
+    pub fn preferred_size(&self) -> Size {
+        if self.vertical {
+            Size::new(30.0, 0.0)
+        } else {
+            Size::new(0.0, 30.0)
+        }
+    }
+
+    pub fn loc(&self) -> Point {
+        self.handle.loc()
+    }
+
+    pub fn set_loc(&mut self, p: Point) {
+        self.handle.set_loc(p);
+        self.vhandle.set_loc(p);
+    }
+
+    pub fn size(&self) -> Size {
+        self.handle.size()
+    }
+
+    pub fn set_size(&mut self, v: Size) {
+        self.handle.set_size(v);
+        self.vhandle.set_size(v);
+    }
+
+    pub fn orient(&self) -> Orient {
+        if self.vertical {
+            Orient::Vertical
+        } else {
+            Orient::Horizontal
+        }
+    }
+
+    pub fn set_orient(&mut self, v: Orient) {
+        let v = matches!(v, Orient::Vertical);
+        if self.vertical != v {
+            if v {
+                self.vhandle.set_pos(self.handle.pos());
+                self.vhandle.set_visible(self.handle.is_visible());
+                self.handle.set_visible(false);
+            } else {
+                self.handle.set_pos(self.vhandle.pos());
+                self.handle.set_visible(self.vhandle.is_visible());
+                self.vhandle.set_visible(false);
+            }
+            self.vertical = v;
+        }
+    }
+
+    pub fn range(&self) -> (usize, usize) {
+        self.handle.range()
+    }
+
+    pub fn set_range(&mut self, min: usize, max: usize) {
+        self.handle.set_range(min, max);
+        self.vhandle.set_range(min, max);
+    }
+
+    pub fn pos(&self) -> usize {
+        if self.vertical {
+            &self.vhandle
+        } else {
+            &self.handle
+        }
+        .pos()
+    }
+
+    pub fn set_pos(&mut self, v: usize) {
+        self.handle.set_pos(v);
+        self.vhandle.set_pos(v);
+    }
+
+    pub async fn wait_change(&self) {
+        if self.vertical {
+            self.vhandle.handle.wait_parent(WM_VSCROLL).await;
+        } else {
+            self.handle.handle.wait_parent(WM_HSCROLL).await;
+        }
+    }
+}
+
+impl AsRawWidget for Slider {
+    fn as_raw_widget(&self) -> RawWidget {
+        if self.vertical {
+            &self.vhandle
+        } else {
+            &self.handle
+        }
+        .as_raw_widget()
+    }
+
+    fn iter_raw_widgets(&self) -> impl Iterator<Item = RawWidget> {
+        [self.handle.as_raw_widget(), self.vhandle.as_raw_widget()].into_iter()
+    }
+}
+
+winio_handle::impl_as_widget!(Slider);
