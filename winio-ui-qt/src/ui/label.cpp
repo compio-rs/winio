1fb0142c5e5f522c7f8d13a959eddc74f83c48f8
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-02-17 16:57:39 +0800

    feat(qt): add link label

---
+++ winio-ui-qt/src/ui/label.cpp
@@ -0,0 +1,11 @@
+#include "label.hpp"
+
+std::unique_ptr<QLabel> new_label(QWidget *parent) {
+    return std::make_unique<QLabel>(parent);
+}
+
+void label_connect_link_activated(QLabel &w, callback_fn_t<void()> callback,
+                                  std::uint8_t const *data) {
+    QObject::connect(&w, &QLabel::linkActivated,
+                     [callback, data](const QString &) { callback(data); });
+}
