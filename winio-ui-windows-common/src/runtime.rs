f9b56f5af3c4dd1a6f029b100305595dd2d02324
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-05-16 23:20:22 +0800

    fix(windows): global multithreaded d2d1 factory

--- winio-ui-windows-common/src/runtime.rs
+++ winio-ui-windows-common/src/runtime.rs
@@ -1,21 +1,16 @@
 #[cfg(feature = "once_cell_try")]
-use std::cell::OnceCell;
+use std::sync::OnceLock;
 
 #[cfg(not(feature = "once_cell_try"))]
-use once_cell::sync::OnceCell;
+use once_cell::sync::OnceCell as OnceLock;
 use windows::Win32::Graphics::Direct2D::{
-    D2D1_FACTORY_TYPE_SINGLE_THREADED, D2D1CreateFactory, ID2D1Factory2,
+    D2D1_FACTORY_TYPE_MULTI_THREADED, D2D1CreateFactory, ID2D1Factory2,
 };
 
-thread_local! {
-    static D2D1_FACTORY: OnceCell<ID2D1Factory2> = const { OnceCell::new() };
-}
+static D2D1_FACTORY: OnceLock<ID2D1Factory2> = const { OnceLock::new() };
 
 pub fn d2d1_factory() -> crate::Result<ID2D1Factory2> {
-    D2D1_FACTORY.with(|d2d1| {
-        d2d1.get_or_try_init(|| unsafe {
-            D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, None)
-        })
+    D2D1_FACTORY
+        .get_or_try_init(|| unsafe { D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, None) })
         .cloned()
-    })
 }
