e4f75c543d7b6fc6c8cde7416503e2af7a718508
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-05-23 01:08:57 +0800

    feat: update makefile for release

--- mac_catalyst.mk
+++ mac_catalyst.mk
@@ -2,8 +2,15 @@ TARGET = aarch64-apple-ios-macabi
 FEATURES = all,nightly,enable_log
 EXAMPLE = widgets
 APP_NAME = Widgets
+PROFILE = debug
 
-BUILD_DIR = target/${TARGET}/debug
+ifeq ($(PROFILE), release)
+	CARGO_FLAGS = --release
+else
+	CARGO_FLAGS = 
+endif
+
+BUILD_DIR = target/${TARGET}/${PROFILE}
 EXAMPLE_BIN = ${BUILD_DIR}/examples/${EXAMPLE}
 APP_BUNDLE = ${BUILD_DIR}/${APP_NAME}.app
 APP_EXEC = ${APP_BUNDLE}/Contents/MacOS/${EXAMPLE}
@@ -14,7 +21,7 @@ INFO_PLIST = winio-ui-ui-kit/Info.plist
 all: bundle
 
 build:
-	cargo build --target ${TARGET} --features ${FEATURES} --example ${EXAMPLE}
+	cargo build --target ${TARGET} --features ${FEATURES} --example ${EXAMPLE} ${CARGO_FLAGS}
 
 bundle: build ${INFO_PLIST}
 	mkdir -p ${APP_BUNDLE}/Contents/MacOS
