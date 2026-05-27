5051a730fe6156dbbfe24898242692587ffbaa06
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-05-28 02:48:17 +0800

    feat(android): example app

---
+++ winio-example/android/app/src/main/java/rs/compio/winio/widgets/MainActivity.java
@@ -0,0 +1,9 @@
+package rs.compio.winio.widgets;
+
+import com.google.androidgamesdk.GameActivity;
+
+public class MainActivity extends GameActivity {
+    static {
+        System.loadLibrary("winio_example");
+    }
+}
