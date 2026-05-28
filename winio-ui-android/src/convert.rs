d69b680b5c0c9f8896b76a98f089044cde8914d4
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-05-29 01:55:02 +0800

    refactor(android): remove unused ext methods

--- winio-ui-android/src/convert.rs
+++ winio-ui-android/src/convert.rs
@@ -1,8 +1,7 @@
 use jni::{
     Env,
-    objects::{JDoubleArray, JIntArray, JObject, JString},
+    objects::{JObject, JString},
 };
-use winio_primitive::{Point, Size};
 
 use crate::Result;
 
@@ -13,57 +12,6 @@ pub trait JObjectExt<O> {
 impl<'a> JObjectExt<String> for JObject<'a> {
     fn to(self, env: &mut Env<'_>) -> Result<String> {
         let s = unsafe { JString::from_raw(env, self.into_raw()) };
-        Ok(s.to_string())
-    }
-}
-
-impl<'a> JObjectExt<Size> for JObject<'a> {
-    fn to(self, env: &mut Env<'_>) -> Result<Size> {
-        let a = unsafe { JDoubleArray::from_raw(env, self.into_raw() as _) };
-        let mut buf = [0f64; 2];
-        a.get_region(env, 0, &mut buf)?;
-
-        Ok(Size {
-            width: buf[0],
-            height: buf[1],
-            _unit: Default::default(),
-        })
-    }
-}
-
-impl<'a> JObjectExt<Point> for JObject<'a> {
-    fn to(self, env: &mut Env<'_>) -> Result<Point> {
-        let a = unsafe { JDoubleArray::from_raw(env, self.into_raw() as _) };
-        let mut buf = [0f64; 2];
-        a.get_region(env, 0, &mut buf)?;
-
-        Ok(Point {
-            x: buf[0],
-            y: buf[1],
-            _unit: Default::default(),
-        })
-    }
-}
-
-impl<'a> JObjectExt<(usize, usize)> for JObject<'a> {
-    fn to(self, env: &mut Env<'_>) -> Result<(usize, usize)> {
-        let a = unsafe { JIntArray::from_raw(env, self.into_raw() as _) };
-        let mut buf = [0i32; 2];
-        a.get_region(env, 0, &mut buf)?;
-
-        Ok((buf[0] as _, buf[1] as _))
-    }
-}
-
-impl<'a> JObjectExt<Option<usize>> for JObject<'a> {
-    fn to(self, env: &mut Env<'_>) -> Result<Option<usize>> {
-        if self.is_null() {
-            return Ok(None);
-        }
-
-        Ok(Some(
-            env.call_method(self, jni::jni_str!("intValue"), jni::jni_sig!("()I"), &[])?
-                .i()? as _,
-        ))
+        Ok(s.try_to_string(env)?)
     }
 }
