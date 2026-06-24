a0ec7f366f7151a8843433f6a6b041d8112068a5
莯凛 <34085039+mokurin000@users.noreply.github.com>
2026-06-24 21:55:28 +0800

    chore: apply follow-ups
    Co-authored-by: Yuyi Wang <Strawberry_Str@hotmail.com>

--- winio/src/widgets/window.rs
+++ winio/src/widgets/window.rs
@@ -72,7 +72,8 @@ impl Window {
 
     /// Set the backdrop effect of the window.
     ///
-    /// On the `win32` backend, backdrop effects may cause rendering artifacts.
+    /// ## Platform specific
+    /// * Win32: backdrop effects may cause rendering artifacts.
     #[cfg(windows)]
     pub fn set_backdrop(&mut self, backdrop: Backdrop) -> Result<()>;
 
