b2ee39d15d158a06952aff5d7daa05febb2d826a
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-08-05 18:30:51 +0800

    refactor(elm): remove PropSource

--- winio-elm/src/bind.rs
+++ winio-elm/src/bind.rs
@@ -4,68 +4,25 @@ use slab::Slab;
 
 use crate::{Component, ComponentSender};
 
-struct PropSource<T> {
-    listeners: Slab<Box<dyn Fn(T)>>,
-}
-
-impl<T> Debug for PropSource<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("PropSource").finish_non_exhaustive()
-    }
-}
-
-impl Default for PropSource<()> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<T> PropSource<T> {
-    pub fn new() -> Self {
-        Self {
-            listeners: Slab::new(),
-        }
-    }
-
-    pub fn bind<C: Component + 'static>(
-        &mut self,
-        current: T,
-        sender: &ComponentSender<C>,
-        f: impl Fn(T) -> C::Message + 'static,
-    ) -> usize {
-        sender.post(f(current));
-        let sender = sender.clone();
-        self.listeners.insert(Box::new(move |value| {
-            let msg = f(value);
-            sender.post(msg);
-        }))
-    }
-
-    pub fn unbind(&mut self, id: usize) {
-        let _ = self.listeners.remove(id);
-    }
-}
-
-impl<T: Clone> PropSource<T> {
-    pub fn notify(&mut self, value: &T) {
-        for (_, listener) in &self.listeners {
-            listener(value.clone());
-        }
-    }
-}
-
-/// A property that can be both a source and a sink.
-#[derive(Debug)]
+/// A property that will notify its listeners when its value changes.
 pub struct Prop<T> {
-    source: PropSource<T>,
+    listeners: Slab<Box<dyn Fn(T)>>,
     value: T,
+}
+
+impl<T: Debug> Debug for Prop<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Prop")
+            .field("value", &self.value)
+            .finish_non_exhaustive()
+    }
 }
 
 impl<T> Prop<T> {
     /// Create a new property with the given initial value.
     pub fn new(value: T) -> Self {
         Self {
-            source: PropSource::new(),
+            listeners: Slab::new(),
             value,
         }
     }
@@ -77,7 +34,7 @@ impl<T> Prop<T> {
 
     /// Unbind a listener by its ID.
     pub fn unbind(&mut self, id: usize) {
-        self.source.unbind(id);
+        let _ = self.listeners.remove(id);
     }
 }
 
@@ -91,7 +48,12 @@ impl<T: Clone + 'static> Prop<T> {
         sender: &ComponentSender<C>,
         f: impl Fn(T) -> C::Message + 'static,
     ) -> usize {
-        self.source.bind(self.value.clone(), sender, f)
+        sender.post(f(self.value.clone()));
+        let sender = sender.clone();
+        self.listeners.insert(Box::new(move |value| {
+            let msg = f(value);
+            sender.post(msg);
+        }))
     }
 }
 
@@ -100,7 +62,9 @@ impl<T: Clone + PartialEq> Prop<T> {
     pub fn set(&mut self, value: T) {
         if value != self.value {
             self.value = value;
-            self.source.notify(&self.value);
+            for (_, listener) in &self.listeners {
+                listener(self.value.clone());
+            }
         }
     }
 }
