e08a9c04e96a84049dd4c144d011eb6517416a77
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-08-10 01:47:56 +0800

    fix(example): rustls on android

--- winio-example/android/app/src/main/java/rs/compio/winio/widgets/MainActivity.java
+++ winio-example/android/app/src/main/java/rs/compio/winio/widgets/MainActivity.java
@@ -1,9 +1,19 @@
 package rs.compio.winio.widgets;
 
+import android.os.Bundle;
+import android.os.StrictMode;
+
 import rs.compio.winio.Activity;
 
 public class MainActivity extends Activity {
     static {
         System.loadLibrary("main");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
+        StrictMode.setThreadPolicy(policy);
     }
 }
