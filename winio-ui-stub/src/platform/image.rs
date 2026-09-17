dd3a211c9d95357ab2e3182e94586942002aa8ac
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-09-18 02:05:39 +0800

    feat(stub): more image APIs

--- winio-ui-stub/src/platform/image.rs
+++ winio-ui-stub/src/platform/image.rs
@@ -1,12 +1,38 @@
 use image::DynamicImage;
 
-use crate::{Result, not_impl};
+use crate::{DrawingContext, DrawingImage, Error, Result, not_impl};
 
 #[derive(Debug)]
 pub struct Image;
 
 impl Image {
-    pub fn new(_image: DynamicImage) -> Result<Self> {
+    pub fn try_clone(&self) -> Result<Self> {
+        not_impl()
+    }
+
+    pub fn try_to_drawing(&self, _context: &DrawingContext) -> Result<DrawingImage> {
+        not_impl()
+    }
+
+    pub fn try_into_drawing(self, _context: &DrawingContext) -> Result<DrawingImage> {
+        not_impl()
+    }
+}
+
+#[allow(clippy::infallible_try_from)]
+impl TryFrom<DynamicImage> for Image {
+    type Error = Error;
+
+    fn try_from(_value: DynamicImage) -> Result<Self, Self::Error> {
+        not_impl()
+    }
+}
+
+#[allow(clippy::infallible_try_from)]
+impl TryFrom<&DynamicImage> for Image {
+    type Error = Error;
+
+    fn try_from(_value: &DynamicImage) -> Result<Self, Self::Error> {
         not_impl()
     }
 }
